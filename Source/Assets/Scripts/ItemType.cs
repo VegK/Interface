daedb2e5d92b903ede32c94f0f15da741a7cae34
VegK <eal.prog@gmail.com>
2015-09-24 22:58:12 +1100

    Класс предмета.
    Сформированный по XSD класс предмета. Типы предмета.

---
+++ Source/Assets/Scripts/ItemType.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// Тип предмета.
+/// </summary>
+public enum ItemType
+{
+	/// <summary>
+	/// Аксессуар.
+	/// </summary>
+	Accessories = 1,
+	/// <summary>
+	/// Броня.
+	/// </summary>
+	Armor = 2,
+	/// <summary>
+	/// Оружие.
+	/// </summary>
+	Weapons = 3
+}
