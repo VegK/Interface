4fcaa629d00b696740d2415e698dac7f46c31979
VegK <eal.prog@gmail.com>
2015-09-25 00:36:46 +1100

    База для инвентаря.
    Класс+префаб инвентаря.
    Добавлен на сцену.

---
+++ Source/Assets/Prefabs/Inventory/InventoryController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryController : MonoBehaviour
+{
+	#region Properties
+	#region Public
+	
+	#endregion
+	#region Private
+
+	#endregion
+	#endregion
+
+	#region Methods
+	#region Public
+
+	#endregion
+	#region Private
+	
+	#endregion
+	#endregion
+}
