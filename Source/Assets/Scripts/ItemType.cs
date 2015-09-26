f53646597f811fb960877f834ea70769a6b58f45
VegK <eal.prog@gmail.com>
2015-09-26 13:46:19 +1100

    Новые типы предметов.

--- Source/Assets/Scripts/ItemType.cs
+++ Source/Assets/Scripts/ItemType.cs
@@ -4,15 +4,31 @@
 public enum ItemType
 {
 	/// <summary>
-	/// Аксессуар.
+	/// Шлем.
 	/// </summary>
-	Accessories = 1,
+	Helmet = 1,
+	/// <summary>
+	/// Перчатки.
+	/// </summary>
+	Gloves = 2,
 	/// <summary>
 	/// Броня.
 	/// </summary>
-	Armor = 2,
+	Shell = 3,
+	/// <summary>
+	/// Аксессуар.
+	/// </summary>
+	Accessories = 4,
 	/// <summary>
 	/// Оружие.
 	/// </summary>
-	Weapons = 3
+	Weapons = 5,
+	/// <summary>
+	/// Ботинки.
+	/// </summary>
+	Boots = 6,
+	/// <summary>
+	/// Щит.
+	/// </summary>
+	Shield = 7
 }
